45ee8618442b39ff5febaef13ec2910a7512aaf7
flaviospjunior <flaviospjunior@gmail.com>
2021-06-27 23:37:20 -0300

    Adicionando funcionalidade tecla TAB

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function addTask(){
     $('.tarefas #tarefa').clone().find('input').val('').end().appendTo('.outrasTarefas')
+    
+};
+
+function teste()
+{
+    $('.tarefas').keydown(function (tecla){
+        if (tecla.keyCode == 9){
+            addTask();
+        } ;
+    })
 }
