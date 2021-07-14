2f24e6ae30ab580cf45752d5568d8791f609898f
flaviospjunior <flaviospjunior@gmail.com>
2021-07-14 01:49:05 -0300

    Alterações de estilo e aplicação do layout flex

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function addTask() {
-  const taskDiv = document.querySelector("#tarefa");
+  const taskDiv = document.querySelector(".tarefas");
   const outrasTarefas = document.querySelector(".outrasTarefas");
+  const tarefa = document.querySelector(".tarefa");
   const clone = taskDiv.cloneNode(true);
   outrasTarefas.appendChild(clone);
 }
